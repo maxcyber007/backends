38ded4a4e271b872a84a474987169fd5f9fcd915
Anuchat  Rungsiyanon <rungsiyanon@gmail.com>
2024-07-19 20:29:03 +0700

    aaaaa

--- src/app/api/getData/route.js
+++ src/app/api/getData/route.js
@@ -14,11 +14,19 @@ pool.connect();
 
 //src/app/api/route.js
 export async function GET() {
+  try {
   const res = await pool.query('SELECT * FROM tbl_student');
   return new Response(JSON.stringify(res.rows), {
     status: 200,
     headers: { 'Content-Type': 'application/json' },
   });
+} catch (error) {
+      console.error(error);
+      return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      });
+}
 }
 
 
