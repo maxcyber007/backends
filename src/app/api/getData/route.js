e9815d697b50fbdc42c9476ae00730cee2e99946
Anuchat  Rungsiyanon <rungsiyanon@gmail.com>
2024-07-19 20:25:29 +0700

    101010

--- src/app/api/getData/route.js
+++ src/app/api/getData/route.js
@@ -11,18 +11,48 @@ const pool = new Pool({
 
 pool.connect();
 
-export async function GET(request) {
-  try {
-    const res = await pool.query('SELECT * FROM tbl_student');
-    return new Response(JSON.stringify(res.rows), {
-      status: 200,
-      headers: { 'Content-Type': 'application/json' },
-    });
-  } catch (error) {
-    console.error(error);
-    return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
+
+//src/app/api/route.js
+export async function GET() {
+  const res = await pool.query('SELECT * FROM tbl_student');
+  return new Response(JSON.stringify(res.rows), {
+    status: 200,
+    headers: { 'Content-Type': 'application/json' },
+  });
 }
+
+
+export async function POST() {
+  return Response.json({
+    message: `POST method called`,
+  });
+}
+
+
+export async function PUT() {
+  return Response.json({
+    message: `PUT method called`,
+  });
+}
+
+
+export async function DELETE() {
+  return Response.json({
+    message: `DELETE method called`,
+  });
+}
+// export async function GET(request) {
+//   try {
+//     const res = await pool.query('SELECT * FROM tbl_student');
+//     return new Response(JSON.stringify(res.rows), {
+//       status: 200,
+//       headers: { 'Content-Type': 'application/json' },
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
+//       status: 500,
+//       headers: { 'Content-Type': 'application/json' },
+//     });
+//   }
+// }
